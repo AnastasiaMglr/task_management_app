1e0bfb1f73a1a28ffa4e07e2648df332cfb8dadc
Anastasia <anastasia.mglr@gmail.com>
2025-09-15 14:55:02 -0400

    app files useless removed

--- task-manager-frontend/src/main.jsx
+++ task-manager-frontend/src/main.jsx
@@ -20,9 +20,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 )
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
